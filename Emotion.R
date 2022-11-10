1c04a583b30d0f58e6291441e294620580a16a17
Bohvh07 <bo0709@hotmail.nl>
2022-11-10 16:20:43 +0100

    sTART ON NRC

--- Emotion.R
+++ Emotion.R
@@ -1,2 +1,8 @@
-####NRC
-library()
+####NRC (sentiment via tidyverse)
+boston_sent <- Boston_final %>%
+  inner_join(get_sentiments("nrc"))
+
+###NRC (via package install.)
+install.packages("devtools")
+install.packages("C:/Users/Bovan/Downloads/NRC-Emotion-Intensity-Lexicon.zip", repos=NULL, type='source')
+install.packages("C:/Users/Bovan/Downloads/NRC-Emotion-Lexicon.zip", repos=NULL, type='source')

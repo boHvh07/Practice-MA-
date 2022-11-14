659bc315b5c52682dbf62c808f372b4c6ec74bd6
Bohvh07 <bo0709@hotmail.nl>
2022-11-14 13:59:58 +0100

    inner join fixed

--- Emotion.R
+++ Emotion.R
@@ -3,9 +3,27 @@ boston_sent <- Boston_final %>%
   inner_join(get_sentiments("nrc"))
 
 ###NRC (via gettext)
-library(textdata)
+NRC.Emotion.Intensity.Lexicon.v1 <- read.delim("~/Downloads/NRC-Emotion-Intensity-Lexicon/NRC-Emotion-Intensity-Lexicon/NRC-Emotion-Intensity-Lexicon-v1.txt", header=FALSE)
+RC.Emotion.Lexicon.Wordlevel.v0.92 <- read.delim("~/Downloads/NRC-Emotion-Lexicon/NRC-Emotion-Lexicon/NRC-Emotion-Lexicon-Wordlevel-v0.92.txt", header=FALSE)
 
-emolex_lexicon <- lexicon_nrc()
-nrceil_lexicon <- lexicon_nrc_eil()
+library(tidyverse)
 
-###NRC(via install package)
+emolex <- NRC.Emotion.Lexicon.Wordlevel.v0.92 %>%
+  rename("word"="V1", "emotion"="V2", "score"="V3")
+nrceil <- NRC.Emotion.Intensity.Lexicon.v1 %>%
+  rename("word"="V1", "emotion"="V2", "score"="V3")
+
+
+group_by(review_num) %>% #group by for sentence polarity
+  summarise(sentiment = sum(score)) # final sentence polarity from words
+
+boston_sent3 <- boston_final%>%
+  inner_join(nrceil)%>% # inner join with our lexicon to get the intensity score
+  group_by(review_num) %>%
+  
+
+
+boston_sent2 <- boston_final%>%
+  inner_join(emolex) %>% # inner join with our lexicon to get the polarity score
+  group_by(review_num) %>% #group by for sentence polarity
+  summarise(sentiment = sum(score)) # final sentence polarity from words

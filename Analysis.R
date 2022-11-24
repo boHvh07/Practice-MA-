5364d0acb19f68436130cbbd82e99533b4ceed59
Bohvh07 <bo0709@hotmail.nl>
2022-11-24 17:43:43 +0100

    Added AIC to the model

--- Analysis.R
+++ Analysis.R
@@ -64,12 +64,22 @@ ols_regress(Rating ~ fear + rating_seen + rating_seen*fear + review_seen + revie
 ols_regress(Rating ~ trust + rating_seen + rating_seen*trust + review_seen + review_seen*trust, data = cityana)
 ols_regress(Rating ~ surp + rating_seen + rating_seen*surp + review_seen + review_seen*surp, data = cityana)
 
-ols_regress(Rating ~ ang + rating_seen + rating_seen*ang + review_seen + review_seen*ang +
+olsdata <- ols_regress(Rating ~ ang + rating_seen + rating_seen*ang + review_seen + review_seen*ang +
               ant + rating_seen*ant + review_seen*ant + joy + rating_seen*joy + review_seen*joy +
               sad + rating_seen*sad + review_seen*sad + dis + rating_seen*dis + review_seen*dis +
               fear + rating_seen*fear + review_seen*fear + trust + rating_seen*trust + review_seen*trust +
               surp + rating_seen*surp + review_seen*surp
-            , data = cityana)
+            , data = cityana_without_outliers)
 
+#### Ordinal Regression (Use polr command)
+library(MASS)
+polrdata <- polr(as.factor(Rating) ~ ang + rating_seen + rating_seen*ang + review_seen + review_seen*ang +
+                    ant + rating_seen*ant + review_seen*ant + joy + rating_seen*joy + review_seen*joy +
+                    sad + rating_seen*sad + review_seen*sad + dis + rating_seen*dis + review_seen*dis +
+                    fear + rating_seen*fear + review_seen*fear + trust + rating_seen*trust + review_seen*trust +
+                    surp + rating_seen*surp + review_seen*surp
+                  , data = cityana_without_outliers, Hess = TRUE, method = c('logistic'), na.rm=T)
 
-### Post test (same but with all reviews of certain restaurants)
+summary(polrdata)
+
+ols_aic(citylm2)

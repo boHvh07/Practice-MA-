2775422d650692fbe392e021b190858bf5f9085b
Bohvh07 <bo0709@hotmail.nl>
2022-11-26 18:38:26 +0100

    Assumptions updated Partial PO model

--- ORD analysis.R
+++ ORD analysis.R
@@ -1,1 +1,71 @@
-ORD Analysis
+library(tidytext)
+library(tidyverse)
+https://www.tidytextmining.com/tfidf.html
+
+city_2 = subset(city_word2, select = -c(word))
+
+city_2 <- city_2 %>%
+  rename("word"="lemma")
+
+#### Term frequency (group by rating / filter per rating can also prove informative)
+word_freq <- city_2 %>%
+  count(city, word, sort=TRUE)
+
+total_words <- word_freq %>% group_by(city) %>% summarize(total = sum(n))
+
+word_freq <- left_join(word_freq, total_words)
+
+freq_rank <- word_freq %>%
+  group_by(city) %>%
+  mutate(rank = row_number(), 
+         `term frequency` = n/total) %>%
+  ungroup()
+#### tf-idf
+restaurant_tf_idf <- word_freq %>%
+  bind_tf_idf(word, city, n)
+
+restaurant_tf_idf2 <- restaurant_tf_idf %>%
+  select(-total) %>%
+  arrange(desc(tf_idf))
+
+### tf-idf per star rating
+rating_words <- city_2 %>%
+  count(Rating, word, sort = TRUE)
+
+plot_rating <- rating_words %>%
+  bind_tf_idf(word, Rating, n) %>%
+  mutate(Rating = factor(Rating, levels = c('1', '2', '3', '4', '5')))
+
+plot_rating %>%
+  arrange(desc(tf_idf))
+
+plot_rating_tf <- plot_rating %>% 
+  group_by(Rating) %>% 
+  slice_max(tf_idf, n = 10) %>% 
+  ungroup() %>%
+  mutate(word = reorder(word, tf_idf)) %>%
+  ggplot(aes(tf_idf, word, fill = Rating)) +
+  geom_col(show.legend = FALSE) +
+  labs(x = "tf-idf", y = NULL) +
+  facet_wrap(~Rating, ncol = 2, scales = "free")
+plot_rating_tf
+
+#### Ordinal Regression (Use polr command)
+library(MASS)
+polrdata <- polr(as.factor(Rating) ~ ang + rating_seen + rating_seen*ang + review_seen + review_seen*ang +
+                   ant + rating_seen*ant + review_seen*ant + joy + rating_seen*joy + review_seen*joy +
+                   sad + rating_seen*sad + review_seen*sad + dis + rating_seen*dis + review_seen*dis +
+                   fear + rating_seen*fear + review_seen*fear + trust + rating_seen*trust + review_seen*trust +
+                   surp + rating_seen*surp + review_seen*surp
+                 , data = cityana_without_outliers, Hess = TRUE, method = c('logistic'), na.rm=T)
+
+summary(polrdata)
+
+ols_aic(citylm2)
+
+##### partial proportional odds model
+library(VGAM)
+
+
+
+
--- ORD assumptions.R
+++ ORD assumptions.R
@@ -1,3 +1,51 @@
+#### remove outliers
+ols_plot_cooksd_chart(citylm, print_plot = T)
+
+cooksD <- cooks.distance(citylm)
+influential <- cooksD[(cooksD > (3 * mean(cooksD, na.rm = TRUE)))]
+influential
+
+names_of_influential <- names(influential)
+outliers <- cityana[names_of_influential,]
+
+cityana_without_outliers <- cityana %>% anti_join(outliers)
+
+citylm2 <- lm(Rating ~ ang + rating_seen + rating_seen*ang + review_seen + review_seen*ang +
+                ant + rating_seen*ant + review_seen*ant + joy + rating_seen*joy + review_seen*joy +
+                sad + rating_seen*sad + review_seen*sad + dis + rating_seen*dis + review_seen*dis +
+                fear + rating_seen*fear + review_seen*fear + trust + rating_seen*trust + review_seen*trust +
+                surp + rating_seen*surp + review_seen*surp
+              , data = cityana_without_outliers)
+
+
 #### Assumption 1: DV is ordinal
 
-### Assumption 2:
+### Assumption 2: IV are continuous, ordinal or interval
+
+### Assumption 3: No multi-collinearity (VIF and correlation)
+citylmnon <- lm(cityana$Rating ~ cityana$avgang + cityana$avgant + cityana$avgdis + cityana$avgjoy + cityana$avgsad
+                + cityana$avgfear + cityana$avgtrust + cityana$avgsurp + cityana$review_seen + cityana$rating_seen, 
+                data = cityana)
+
+library(car)
+vif(citylmnon)
+
+vif_values <- vif(citylmnon)
+barplot(vif_values, main = "VIF Values", horiz = TRUE, col = "steelblue")
+abline(v = 5, lwd = 3, lty = 2)
+
+
+citycor <- cityana_without_outliers[ ,c('Rating', 'avgang', 'avgant', 'avgdis', 'avgjoy', 'avgsad'
+                                        , 'avgfear', 'avgtrust', 'avgsurp', 'review_seen', 'rating_seen')]
+cor(citycor, use = 'complete.obs')
+
+
+### Assumption 4: Proportional odds (Brant's test)
+cityord <- polr(as.factor(Rating) ~ avgang + avgant + avgdis + avgjoy + avgsad
+                + avgfear + avgtrust + avgsurp + review_seen + rating_seen, 
+                data = cityana_without_outliers, Hess = TRUE, method = c('logistic'))
+
+library(brant)
+brant(cityord)
+
+Assumption is violated -> partial proportional odds models

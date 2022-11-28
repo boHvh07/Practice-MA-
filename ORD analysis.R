e64fa12ae21c940f1cdbc1209aafda359d627e6e
Bohvh07 <bo0709@hotmail.nl>
2022-11-29 00:02:41 +0100

    extra work done on analysis

--- ORD analysis.R
+++ ORD analysis.R
@@ -52,20 +52,69 @@ plot_rating_tf
 
 #### Ordinal Regression (Use polr command)
 library(MASS)
-polrdata <- polr(as.factor(Rating) ~ ang + rating_seen + rating_seen*ang + review_seen + review_seen*ang +
-                   ant + rating_seen*ant + review_seen*ant + joy + rating_seen*joy + review_seen*joy +
-                   sad + rating_seen*sad + review_seen*sad + dis + rating_seen*dis + review_seen*dis +
-                   fear + rating_seen*fear + review_seen*fear + trust + rating_seen*trust + review_seen*trust +
-                   surp + rating_seen*surp + review_seen*surp
-                 , data = cityana_without_outliers, Hess = TRUE, method = c('logistic'), na.rm=T)
+polrdata <- polr(as.factor(Rating) ~ avgang + avgant + avgdis + avgjoy + avgsad
+                 + avgfear + avgtrust + avgsurp + review_seen + rating_seen + avgang*review_seen + avgant*review_seen + avgdis*review_seen +
+                   avgjoy*review_seen + avgsad*review_seen + avgfear*review_seen + avgtrust*review_seen + avgsurp*review_seen +
+                   avgang*rating_seen + avgant*rating_seen + avgdis*rating_seen + avgjoy*rating_seen + avgsad*rating_seen + 
+                   avgfear*rating_seen + avgtrust*rating_seen + avgsurp*rating_seen,
+                 data = cityana_without_outliers, Hess = TRUE, method = c('logistic'))
 
 summary(polrdata)
 
 ols_aic(citylm2)
 
 ##### partial proportional odds model
-library(VGAM)
+ppo.model <- vglm(Rating ~ avgang + avgant + avgdis + avgjoy + avgsad
+                                      + avgfear + avgtrust + avgsurp + review_seen + rating_seen 
+                                      ,data = cityana_without_outliers, family = cumulative(parallel = F~avgant))
+head(fitted(ppo.model))
+
+summary(ppo.model)
+
+ppo.model2 <- vglm(Rating ~ avgang + avgant + avgdis + avgjoy + avgsad
+                  + avgfear + avgtrust + avgsurp + review_seen + rating_seen + avgang*review_seen + avgant*review_seen + avgdis*review_seen +
+                    avgjoy*review_seen + avgsad*review_seen + avgfear*review_seen + avgtrust*review_seen + avgsurp*review_seen +
+                    avgang*rating_seen + avgant*rating_seen + avgdis*rating_seen +
+                    avgjoy*rating_seen + avgsad*rating_seen + avgfear*rating_seen + avgtrust*rating_seen + avgsurp*rating_seen
+                  ,data = cityana_without_outliers, family = cumulative(parallel = T~avgant))
+
+summary(ppo.model2)
+
+ppo.model3 <- vglm(Rating ~ ang + rating_seen + rating_seen*ang + review_seen + review_seen*ang
+                  ,data = cityana_without_outliers, family = cumulative(parallel = F))
+
+summary(ppo.model3)
+
+ctable <- coef(summary(ppo.model))
+
+ci <- confint(ppo.model)
+ci 
+
+exp(coef(ppo.model))
+
+exp(cbind(OR = coef(ppo.model), ci))
+
+coef(ppo.model(matrix = TRUE))
+
+
+#### partial nonproportional odds model
+ppo.model4 <- vglm(Rating ~ avgang + avgant + avgdis + avgjoy + avgsad
+                   + avgfear + avgtrust + avgsurp + review_seen + rating_seen + avgang*review_seen + avgant*review_seen + avgdis*review_seen +
+                     avgjoy*review_seen + avgsad*review_seen + avgfear*review_seen + avgtrust*review_seen + avgsurp*review_seen +
+                     avgang*rating_seen + avgant*rating_seen + avgdis*rating_seen +
+                     avgjoy*rating_seen + avgsad*rating_seen + avgfear*rating_seen + avgtrust*rating_seen + avgsurp*rating_seen
+                   ,data = cityana_without_outliers, family = propodds)
+
+summary(ppo.model4)
+coef(ppo.model4, matrix = TRUE)
+confint(ppo.model4, matrix = TRUE)
+exp(coef(ppo.model4, matrix = TRUE))
+exp(confint(ppo.model4, matrix = TRUE))
+cbind(exp(coef(ppo.model4)), exp(confint(ppo.model4)))
 
 
 
 
+
+####Marginal effects table after model summary
+summary(cityana_without_outliers)

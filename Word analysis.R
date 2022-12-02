f3a6bcfbe9ee9ec6281cc063565a02427c606bf3
Bohvh07 <bo0709@hotmail.nl>
2022-12-02 18:54:30 +0100

    Most freq words per rating graph

--- Word analysis.R
+++ Word analysis.R
@@ -1,18 +1,20 @@
 library(tidytext)
 library(tidyverse)
+library(ggplot2)
+library(ggpubr)
 
-city_2.2 = subset(city_1, select = -c(word))
+city_2 = subset(city_1, select = -c(word))
 
-city_2.2 <- city_2.2 %>%
+city_2 <- city_2 %>%
   rename("word"="lemma")
 
 #### Term frequency (group by rating / filter per rating can also prove informative)
-word_freq2 <- city_2.2 %>%
+word_freq <- city_2.2 %>%
   count(city, word, sort=TRUE)
 
-total_words2 <- word_freq %>% group_by(city) %>% summarize(total = sum(n))
+total_words <- word_freq %>% group_by(city) %>% summarize(total = sum(n))
 
-word_freq3 <- left_join(word_freq2, total_words2)
+word_freq2 <- left_join(word_freq, total_words)
 
 freq_rank <- word_freq3 %>%
   group_by(city) %>%
@@ -22,16 +24,107 @@ freq_rank <- word_freq3 %>%
 
 write.csv(freq_rank, "freq_rank.csv")
 
+rating_words <- city_2 %>%
+  count(Rating, word, sort = TRUE)
+  
+rank1 <- subset(rating_words, Rating == 1)
+rank2 <- subset(rating_words, Rating == 2)
+rank3 <- subset(rating_words, Rating == 3)
+rank4 <- subset(rating_words, Rating == 4)
+rank5 <- subset(rating_words, Rating == 5)
+
+rank1 <- rank1 %>% 
+  slice_max(n, n = 10) %>%
+  mutate(word = reorder(word, n)) %>% 
+  ggplot(aes(word, n)) + 
+  geom_col() +
+  coord_flip() +
+  labs(x = NULL, y = NULL, title = '1 star rating \n') +
+  geom_text(aes(label = n), hjust = 1.2, colour = "white", fontface = "bold")
+
+rank2 <- rank2 %>% 
+  slice_max(n, n = 10) %>%
+  mutate(word = reorder(word, n)) %>% 
+  ggplot(aes(word, n)) + 
+  geom_col() +
+  coord_flip() +
+  labs(x = NULL, y = NULL, title = '2 star rating \n') +
+  geom_text(aes(label = n), hjust = 1.2, colour = "white", fontface = "bold")
+
+rank3 <- rank3 %>% 
+  slice_max(n, n = 10) %>%
+  mutate(word = reorder(word, n)) %>% 
+  ggplot(aes(word, n)) + 
+  geom_col() +
+  coord_flip() +
+  labs(x = NULL, y = NULL, title = '3 star rating \n') +
+  geom_text(aes(label = n), hjust = 1.2, colour = "white", fontface = "bold")
+
+rank4 <- rank4 %>% 
+  slice_max(n, n = 10) %>%
+  mutate(word = reorder(word, n)) %>% 
+  ggplot(aes(word, n)) + 
+  geom_col() +
+  coord_flip() +
+  labs(x = NULL, y = NULL, title = '4 star rating \n') +
+  geom_text(aes(label = n), hjust = 1.2, colour = "white", fontface = "bold")
+
+rank5 <- subset(rating_words, Rating == 5)
+
+rank5 <- rank5 %>% 
+  slice_max(n, n = 10) %>%
+  mutate(word = reorder(word, n)) %>% 
+  ggplot(aes(word, n)) + 
+  geom_col(show.legend = FALSE) +
+  coord_flip() +
+  labs(x = NULL, y = NULL, title = '5 star rating \n') +
+  geom_text(aes(label = n), hjust = 1.2, colour = "white", fontface = "bold") +
+  theme()
+
+rank_figure <- ggarrange(rank1, rank2, rank3, rank4, rank5,
+                         ncol = 2, nrow = 3)
+annotate_figure(rank_figure, top = text_grob("Most frequent used words per restaurant rating",
+                                             color = "black", face = 'bold', size = 14))
+ggsave("rank_figure.pdf")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #### tf-idf
-restaurant_tf_idf <- word_freq3 %>%
+restaurant_tf_idf <- word_freq2 %>%
   bind_tf_idf(word, city, n)
 
 restaurant_tf_idf2 <- restaurant_tf_idf %>%
   select(-total) %>%
   arrange(desc(tf_idf))
 
+
 ### tf-idf per star rating
-rating_words <- city_2.2 %>%
+rating_words <- city_2 %>%
   count(Rating, word, sort = TRUE)
 
 plot_rating <- rating_words %>%
@@ -41,7 +134,7 @@ plot_rating <- rating_words %>%
 plot_rating %>%
   arrange(desc(tf_idf))
 
-plot_rating_tf <- plot_rating %>% 
+plot_rating_freq <- plot_rating %>% 
   group_by(Rating) %>% 
   slice_max(tf_idf, n = 10) %>% 
   ungroup() %>%
